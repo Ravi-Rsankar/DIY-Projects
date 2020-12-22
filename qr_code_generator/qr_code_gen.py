f27e1a95d6bc51f23ecebe2b998cb5278f593ac3
Ravi-Rsankar <ravisankar.r93@gmail.com>
2020-12-22 19:42:13 +0530

    QR-Code generator

--- qr_code_generator/qr_code_gen.py
+++ qr_code_generator/qr_code_gen.py
@@ -2,10 +2,10 @@
 import pyqrcode
 
 # define the data
-data = 'http://bhc.edu.in/'
+data = 'https://github.com/Ravi-Rsankar'
 
 # create qrcode
 img = pyqrcode.create(data)
 
 # save the qrcode in png format with proper scaling
-img.png('bi.png', scale=10)
+img.png('github.png', scale=10)
